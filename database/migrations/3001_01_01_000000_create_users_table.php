4e5f5feb80b6fc83cc1f8fe811ceea17e8ca7542
rasheedgavin <rasheedgavinesponga@gmail.com>
2025-03-03 17:34:49 +0800

    inalis ko lang comment sir

--- database/migrations/3001_01_01_000000_create_users_table.php
+++ database/migrations/3001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            // $table->foreignId('role_id')->default(0)->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->default(0)->constrained()->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
